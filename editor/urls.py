b56c9d05dd06143cc8f1d4adbe152de8a05affe0
yueyoum <yueyoum@gmail.com>
2015-04-23 17:08:19 +0800

    download zip

--- editor/urls.py
+++ editor/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'editor.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'zip/$', views.download_zip)
 ]
---
+++ views.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Author:         Wang Chao <yueyoum@gmail.com>
+Filename:       views
+Date Created:   2015-04-23 16:42
+Description:
+
+"""
+import datetime
+
+from cStringIO import StringIO
+import zipfile
+
+from django.http import HttpResponse
+from django.core import management
+
+
+class InMemoryZip(object):
+    def __init__(self):
+        self.buffer = StringIO()
+
+    def add(self, filename, content):
+        f = zipfile.ZipFile(self.buffer, mode='a')
+        f.writestr(filename, content)
+
+    def read(self):
+        return self.buffer.getvalue()
+
+
+
+def create_fixture(model):
+    buffer = StringIO()
+    management.call_command('dumpdata', model, format='xml', indent=4, stdout=buffer)
+    return buffer.getvalue()
+
+
+def download_zip(request):
+    models = [
+        ('errormsg.ErrorMsg', 'errormsg.xml'),
+    ]
+
+    memzip = InMemoryZip()
+    for model, filename in models:
+        fixture = create_fixture(model)
+        memzip.add(filename, fixture)
+
+    now = datetime.datetime.now().strftime("%Y-%M-%d-%H-%m-%S")
+
+    response = HttpResponse(memzip.read(), content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename="config_{0}.zip"'.format(now)
+
