6f78cf0925b00801de6de43dc308bb8f95882492
yueyoum <yueyoum@gmail.com>
2015-04-23 14:40:34 +0800

    first

---
+++ apps/errormsg/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from apps.errormsg.models import ErrorMsg
+
+@admin.register(ErrorMsg)
+class ErrorMsgAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'error_index', 'text_zh'
+    )
---
+++ apps/errormsg/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+class ErrorMsg(models.Model):
+    id = models.IntegerField(primary_key=True)
+    error_index = models.CharField(max_length=64, unique=True)
+    text_zh = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'error_msg'
+        verbose_name = '错误代码'
+        verbose_name_plural = '错误代码'
