b1bc1ad7275b1ed945a182f55fc854961ebbd61d
yueyoum <yueyoum@gmail.com>
2015-07-13 18:20:21 +0800

    adjust unit des

--- apps/unit/models.py
+++ apps/unit/models.py
@@ -50,7 +50,7 @@ class Unit(models.Model):
         for f in fixture:
 
             des = {
-                int(d.policy.id): [line.split('|') for line in d.des.split('\n')] for d in cls.objects.get(id=f['pk']).des.all()
+                int(d.policy.id): [line.split('|') for line in d.des.strip('\r\n').split('\r\n')] for d in cls.objects.get(id=f['pk']).des.all()
             }
 
             f['fields']['des'] = des
