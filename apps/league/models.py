ed035972cd5e0d38eaa2a5a723b7a11ab16fffd1
yueyoum <yueyoum@gmail.com>
2015-09-10 10:15:25 +0800

    x

--- apps/league/models.py
+++ apps/league/models.py
@@ -6,7 +6,8 @@ class League(models.Model):
     id = models.IntegerField(primary_key=True, verbose_name="等级")
     name = models.CharField(max_length=255, verbose_name="名字")
 
-    day_reward = models.ForeignKey('package.Package', related_name="league_day_reward", verbose_name="日奖励")
+    day_reward = models.ForeignKey('package.Package', related_name="league_day_reward", verbose_name="日奖励（赢）")
+    day_reward_lose = models.ForeignKey('package.Package', null=True, blank=True, related_name="league_day_reward_lose", verbose_name="日奖励（输）")
     week_reward = models.ForeignKey('package.Package', related_name="league_week_reward", verbose_name="周奖励")
 
     def __unicode__(self):
@@ -16,4 +17,3 @@ class League(models.Model):
         db_table = 'league'
         verbose_name = "联赛"
         verbose_name_plural = "联赛"
-
