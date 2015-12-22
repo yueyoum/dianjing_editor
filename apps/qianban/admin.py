8cacd5f88e6853b42e187b92559c55534a34c71b
yueyoum <yueyoum@gmail.com>
2015-12-22 16:11:47 +0800

    admin

--- apps/qianban/admin.py
+++ apps/qianban/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from apps.qianban.models import QianBan
 
+
+class QianBanResource(resources.ModelResource):
+    class Meta:
+        model = QianBan
+
 @admin.register(QianBan)
-class QianBanAdmin(admin.ModelAdmin):
+class QianBanAdmin(ImportExportModelAdmin):
+    resource_class = QianBanResource
+
     list_display = ('id', 'name', 'des',
                     'condition_tp', 'condition_value',
                     'addition_tp', 'addition_value'
