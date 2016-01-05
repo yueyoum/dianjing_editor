8a1ab90be775157e6dd3178ab94b9eb8ec3c62b0
yueyoum <yueyoum@gmail.com>
2016-01-05 16:49:39 +0800

    item

--- apps/item/admin.py
+++ apps/item/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from apps.item.models import Item
 
+
+class ItemResource(resources.ModelResource):
+    class Meta:
+        model = Item
+
 @admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
+class ItemAdmin(ImportExportModelAdmin):
+    resource_class = ItemResource
+
     list_display = (
         'id', 'tp', 'sub_tp', 'name', 'icon', 'quality',
         'des',
@@ -14,3 +23,5 @@ class ItemAdmin(admin.ModelAdmin):
     )
 
     list_filter = ('tp',)
+
+    change_list_template = 'item_change_list.html'
