3baac02bb069aab082cecdd082760e7105b61422
yueyoum <yueyoum@gmail.com>
2016-02-24 15:51:56 +0800

    adjust

--- apps/unit/models.py
+++ apps/unit/models.py
@@ -163,6 +163,7 @@ class UnitNew(models.Model):
     dodge_rate = models.DecimalField(max_digits=4, decimal_places=4, verbose_name='闪避率')
     crit_rate = models.DecimalField(max_digits=4, decimal_places=4, verbose_name='暴击率')
     toughness_rate = models.DecimalField(max_digits=4, decimal_places=4, verbose_name='韧性')
+    crit_multiple = models.DecimalField(max_digits=8, decimal_places=4, verbose_name='暴击被率')
 
     hurt_addition_to_terran = models.DecimalField(max_digits=8, decimal_places=4, verbose_name='对人族伤害加成')
     hurt_addition_to_protoss = models.DecimalField(max_digits=8, decimal_places=4, verbose_name='对神族伤害加成')
