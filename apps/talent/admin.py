52c6924bfc117b17451007d0806fdef94e310dd5
said696 <said047@163.com>
2016-04-14 02:23:39 -0700

    modify talent module

--- apps/talent/admin.py
+++ apps/talent/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from apps.talent.models import Talent
 
 # Register your models here.
 
 
+class ResourceTalent(resources.ModelResource):
+    class Meta:
+        model = Talent
+
+
 @admin.register(Talent)
-class TalentAdmin(admin.ModelAdmin):
-    list_display = [
+class TalentAdmin(ImportExportModelAdmin):
+    resource_class = ResourceTalent
+
+    list_display = (
             "id", "tp", "position", "name", "des",
             "image", "unlock", "up_need", "effect_id",
-        ]
+    )
