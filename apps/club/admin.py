4458dd46c2cd1d2e6c52ec39970bf101f769eda6
yueyoum <yueyoum@gmail.com>
2016-04-27 15:28:23 +0800

    admin

--- apps/club/admin.py
+++ apps/club/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from apps.club.models import ClubFlag, ClubLevel
 
+class ResourceClubFlag(resources.ModelResource):
+    class Meta:
+        model = ClubFlag
+
+class ResourceClubLevel(resources.ModelResource):
+    class Meta:
+        model = ClubLevel
+
+
 @admin.register(ClubFlag)
-class ClubFlagAdmin(admin.ModelAdmin):
+class ClubFlagAdmin(ImportExportModelAdmin):
+    resource_class = ResourceClubFlag
     list_display = ('id', 'flag')
 
 @admin.register(ClubLevel)
-class ClubLevelAdmin(admin.ModelAdmin):
+class ClubLevelAdmin(ImportExportModelAdmin):
+    resource_class = ResourceClubLevel
     list_display = ('id', 'exp', 'des')
