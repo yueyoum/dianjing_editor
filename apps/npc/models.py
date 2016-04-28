3a8dc00209de78e9dfe9012cb0bef909c9fded27
yueyoum <yueyoum@gmail.com>
2016-04-29 01:16:06 +0800

    adjust

--- apps/npc/models.py
+++ apps/npc/models.py
@@ -11,3 +11,17 @@ class NPCFormation(models.Model):
         db_table = 'npc_formation'
         verbose_name = 'NPC阵型'
         verbose_name_plural = "NPC阵型"
+
+
+    @classmethod
+    def patch_fixture(cls, fixture):
+        def _parse_staffs(text):
+            result = []
+            for x in text.split(';'):
+                _a, _b, _c = x.split(',')
+                result.append((int(_a), int(_b), int(_c)))
+
+        for f in fixture:
+            f['fields']['staffs'] = _parse_staffs(f['fields']['staffs'])
+
+        return fixture
