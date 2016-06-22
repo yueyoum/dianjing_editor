0bd6550726167a7f0c0f2d810eb845294fddb09a
yueyoum <yueyoum@gmail.com>
2016-06-22 14:39:26 +0800

    x

--- apps/task/models.py
+++ apps/task/models.py
@@ -103,6 +103,7 @@ class TaskMain(models.Model):
 class TaskCondition(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
+    scene = models.CharField(max_length=255, blank=True)
     ui = models.CharField(max_length=255, blank=True)
     server_module = models.CharField(max_length=255, blank=True)
 
