de6aa3ff4fb36db56a13bb91440e7836f209e75e
yueyoum <yueyoum@gmail.com>
2016-10-20 14:31:56 +0800

    x

--- apps/config/admin.py
+++ apps/config/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from apps.config.models import ClientConfig, GlobalConfig
 
+class ResourceCC(resources.ModelResource):
+    class Meta:
+        model = ClientConfig
+        bulk_replace = True
+
+class ResourceGC(resources.ModelResource):
+    class Meta:
+        model = GlobalConfig
+        bulk_replace = True
+
 @admin.register(ClientConfig)
-class ClientConfigAdmin(admin.ModelAdmin):
+class ClientConfigAdmin(ImportExportModelAdmin):
+    resource_class = ResourceCC
     list_display = ('id', 'value', 'mean')
 
 @admin.register(GlobalConfig)
-class GlobalConfigAdmin(admin.ModelAdmin):
+class GlobalConfigAdmin(ImportExportModelAdmin):
+    resource_class = ResourceGC
     list_display = ('id', 'value', 'mean')
--- apps/errormsg/admin.py
+++ apps/errormsg/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from apps.errormsg.models import ErrorMsg
 
+class ResourceEM(resources.ModelResource):
+    class Meta:
+        model = ErrorMsg
+        bulk_replace = True
+
 @admin.register(ErrorMsg)
-class ErrorMsgAdmin(admin.ModelAdmin):
+class ErrorMsgAdmin(ImportExportModelAdmin):
+    resource_class = ResourceEM
     list_display = (
         'id', 'error_index', 'text_zh', 'is_retry', 'jump_to', 'des'
     )
